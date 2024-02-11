9cce383e7351b80fb20f1a2ab846fca863bae799
Khai Do <zaro0508@gmail.com>
2024-02-10 19:53:28 -0800

    setup poetry and move to github actions
    Update with files from the  sceptre-resolver-template[1]
    repo to update this project to use poetry and github actions CI
    
    [1] https://github.com/Sceptre/sceptre-resolver-template

---
+++ .github/PULL_REQUEST_TEMPLATE.md
@@ -0,0 +1,21 @@
+[Your PR description here]
+
+## PR Checklist
+
+- [ ] Wrote a good commit message & description [see guide below].
+- [ ] Commit message starts with `[Resolve #issue-number]`.
+- [ ] Added/Updated unit tests.
+- [ ] Added/Updated integration tests (if applicable).
+- [ ] All unit tests (`poetry run tox`) are passing.
+- [ ] Used the same coding conventions as the rest of the project.
+- [ ] The new code passes pre-commit validations (`poetry run pre-commit run --all-files`).
+- [ ] The PR relates to _only_ one subject with a clear title.
+      and description in grammatically correct, complete sentences.
+
+## Approver/Reviewer Checklist
+
+- [ ] Before merge squash related commits.
+
+## Other Information
+
+[Guide to writing a good commit](http://chris.beams.io/posts/git-commit/)
--- tests/test_ssm.py
+++ tests/test_ssm.py
@@ -3,82 +3,71 @@
 import pytest
 from mock import MagicMock, patch, sentinel
 
-from botocore.exceptions import ClientError
-
 from sceptre.connection_manager import ConnectionManager
 from sceptre.stack import Stack
 
 from resolver.ssm import SSM, SsmBase
-from resolver.exceptions import ParameterNotFoundError
 
 
-region = 'us-east-1'
+region = "us-east-1"
 
 
 class TestSsmResolver(object):
     def test_resolve_str_arg_no_param_name(self):
+        #stack = Stack(name="foo",project_code="mar",region="us-east-1")
         stack = MagicMock(spec=Stack)
+        stack.name = "test_stack"
         stack.profile = "test_profile"
         stack.dependencies = []
         stack._connection_manager = MagicMock(spec=ConnectionManager)
-        stack_ssm_resolver = SSM(
-            None, stack
-        )
+        stack_ssm_resolver = SSM(None, stack)
         with pytest.raises(ValueError):
             stack_ssm_resolver.resolve()
 
     def test_resolve_obj_arg_no_param_name(self):
         stack = MagicMock(spec=Stack)
+        stack.name = "test_stack"
         stack.profile = "test_profile"
         stack.dependencies = []
         stack._connection_manager = MagicMock(spec=ConnectionManager)
-        stack_ssm_resolver = SSM(
-            {}, stack
-        )
+        stack_ssm_resolver = SSM({}, stack)
         with pytest.raises(ValueError):
             stack_ssm_resolver.resolve()
 
-    @patch(
-        "resolver.ssm.SSM._get_parameter_value"
-    )
+    @patch("resolver.ssm.SSM._get_parameter_value")
     def test_resolve_str_arg(self, mock_get_parameter_value):
         stack = MagicMock(spec=Stack)
+        stack.name = "test_stack"
         stack.profile = "test_profile"
         stack.region = region
         stack.dependencies = []
         stack._connection_manager = MagicMock(spec=ConnectionManager)
-        stack_ssm_resolver = SSM(
-            "/dev/DbPassword", stack
-        )
+        stack_ssm_resolver = SSM("/dev/DbPassword", stack)
         mock_get_parameter_value.return_value = "parameter_value"
         stack_ssm_resolver.resolve()
         mock_get_parameter_value.assert_called_once_with(
             "/dev/DbPassword", region, "test_profile"
         )
 
-    @patch(
-        "resolver.ssm.SSM._get_parameter_value"
-    )
+    @patch("resolver.ssm.SSM._get_parameter_value")
     def test_resolve_obj_arg_no_profile(self, mock_get_parameter_value):
         stack = MagicMock(spec=Stack)
+        stack.name = "test_stack"
         stack.profile = "test_profile"
         stack.region = region
         stack.dependencies = []
         stack._connection_manager = MagicMock(spec=ConnectionManager)
-        stack_ssm_resolver = SSM(
-            {"name": "/dev/DbPassword"}, stack
-        )
+        stack_ssm_resolver = SSM({"name": "/dev/DbPassword"}, stack)
         mock_get_parameter_value.return_value = "parameter_value"
         stack_ssm_resolver.resolve()
         mock_get_parameter_value.assert_called_once_with(
             "/dev/DbPassword", region, "test_profile"
         )
 
-    @patch(
-        "resolver.ssm.SSM._get_parameter_value"
-    )
+    @patch("resolver.ssm.SSM._get_parameter_value")
     def test_resolve_obj_arg_profile_override(self, mock_get_parameter_value):
         stack = MagicMock(spec=Stack)
+        stack.name = "test_stack"
         stack.profile = "test_profile"
         stack.region = region
         stack.dependencies = []
@@ -92,115 +81,28 @@ class TestSsmResolver(object):
             "/dev/DbPassword", region, "new_profile"
         )
 
-    @patch(
-        "resolver.ssm.SSM._get_parameter_value"
-    )
+    @patch("resolver.ssm.SSM._get_parameter_value")
     def test_resolve_obj_arg_region_override(self, mock_get_parameter_value):
         stack = MagicMock(spec=Stack)
+        stack.name = "test_stack"
         stack.profile = "test_profile"
         stack.region = region
         stack.dependencies = []
         stack._connection_manager = MagicMock(spec=ConnectionManager)
 
-        custom_region = 'ca-central-1'
+        custom_region = "ca-central-1"
         assert custom_region != region
 
         stack_ssm_resolver = SSM(
             {
                 "name": "/dev/DbPassword",
                 "region": custom_region,
-                "profile": "new_profile"
+                "profile": "new_profile",
             },
-            stack
+            stack,
         )
         mock_get_parameter_value.return_value = "parameter_value"
         stack_ssm_resolver.resolve()
         mock_get_parameter_value.assert_called_once_with(
             "/dev/DbPassword", custom_region, "new_profile"
         )
-
-
-class MockSsmBase(SsmBase):
-    """
-    MockBaseResolver inherits from the abstract base class
-    SsmBase, and implements the abstract methods. It is used
-    to allow testing on SsmBase, which is not otherwise
-    instantiable.
-    """
-
-    def __init__(self, *args, **kwargs):
-        super(MockSsmBase, self).__init__(*args, **kwargs)
-
-    def resolve(self):
-        pass
-
-
-class TestSsmBase(object):
-
-    def setup_method(self, test_method):
-        self.stack = MagicMock(spec=Stack)
-        self.stack.name = "test_name"
-        self.stack._connection_manager = MagicMock(
-            spec=ConnectionManager
-        )
-        self.base_ssm = MockSsmBase(
-            None, self.stack
-        )
-
-    @patch(
-        "resolver.ssm.SsmBase._request_parameter"
-    )
-    def test_get_parameter_value_with_valid_key(self, mock_request_parameter):
-        mock_request_parameter.return_value = {
-            "Parameter": {
-                "Name": "/dev/DbPassword",
-                "Type": "SecureString",
-                "Value": "Secret",
-                "Version": 1,
-                "LastModifiedDate": 1531863312.945,
-                "ARN": "arn:aws:ssm:us-east-1:111111111111:parameter/dev/DbPassword"
-            }
-        }
-        response = self.base_ssm._get_parameter_value("/dev/DbPassword", region)
-        assert response == "Secret"
-
-    @patch(
-        "resolver.ssm.SsmBase._request_parameter"
-    )
-    def test_get_parameter_value_with_invalid_response(self, mock_request_parameter):
-        mock_request_parameter.return_value = {
-            "Parameter": {
-                "Name": "/dev/DbPassword"
-            }
-        }
-
-        with pytest.raises(KeyError):
-            self.base_ssm._get_parameter_value(None, region)
-
-    def test_request_parameter_with_unkown_boto_error(self):
-        self.stack.connection_manager.call.side_effect = ClientError(
-            {
-                "Error": {
-                    "Code": "500",
-                    "Message": "Boom!"
-                }
-            },
-            sentinel.operation
-        )
-
-        with pytest.raises(ClientError):
-            self.base_ssm._request_parameter(None, region)
-
-    def test_request_parameter_with_parameter_not_found(self):
-        self.stack.connection_manager.call.side_effect = ClientError(
-            {
-                "Error": {
-                    "Code": "ParameterNotFound",
-                    "Message": "Boom!"
-                }
-            },
-            sentinel.operation
-        )
-
-        with pytest.raises(ParameterNotFoundError):
-            self.base_ssm._request_parameter(None, region)
